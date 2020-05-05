cf39a4e9446db7c19ce0900cbe349a5d1c581c47
Chris Mague <github@mague.com>
2020-05-05 11:44:03 -0700

    use local files instead

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ data "template_file" "tmux" {
   }
 }
 
-resource "null_resource" "tmux" {
-  provisioner "local-exec" {
-     command = "echo \"${data.template_file.tmux.rendered}\" > ${local.tmux_path}/${var.outfile} && chmod +x ${local.tmux_path}/${var.outfile} "
-  }
-  depends_on = ["data.template_file.tmux"]
+resource "local_file" "tmux" {
+  content     = "${data.template_file.tmux.rendered}"
+  filename = "${local.tmux_path}/${var.outfile}"
+  file_permission = "0755"
 }
+
