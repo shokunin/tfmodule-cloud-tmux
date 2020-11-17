7af6a446da64657ff6609f87d2bc5a82e69f90ed
Chris Mague <github@mague.com>
2020-11-17 12:31:11 -0800

    update to be compatable after 0.13

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 locals {
-  tmux_path = (var.outpath == "" ? "${path.module}" : "${var.outpath}")
+  tmux_path = (var.outpath == "" ? path.module : var.outpath)
 }
 
 data "template_file" "tmux" {
-  template = "${file("${path.module}/tmux.ctmpl")}"
+  template = file("${path.module}/tmux.ctmpl")
   vars = {
-      session-name = var.session-name
-      nodes        = join(" ", var.nodes)
-      ssh-key      = var.ssh-key
-      ssh-args     = var.ssh-args
-      user         = var.user
+    session-name = var.session-name
+    nodes        = join(" ", var.nodes)
+    ssh-key      = var.ssh-key
+    ssh-args     = var.ssh-args
+    user         = var.user
   }
 }
 
 resource "local_file" "tmux" {
-  content     = "${data.template_file.tmux.rendered}"
-  filename = "${local.tmux_path}/${var.outfile}"
+  content         = data.template_file.tmux.rendered
+  filename        = "${local.tmux_path}/${var.outfile}"
   file_permission = "0755"
 }
 
